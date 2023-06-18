0b8ded5934519625a79fabc973e9313c7943216e
aldous huxley <root+github@99mvps.dev>
2023-06-18 00:33:53 -0300

    fix removing path resolution to work on vercel

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,13 @@ import { ConfigModule } from "@nestjs/config";
 import { APP_FILTER } from "@nestjs/core";
 // import { TypeOrmModule } from "@nestjs/typeorm";
 
-import { AppController } from "@/app.controller";
-import { AppConfiguration } from "@/config";
-import { envFile } from "@/config/env";
+import { AppController } from "./app.controller";
+import { AppConfiguration } from "./config";
+import { envFile } from "./config/env";
+import { GlobalCatcher, HttpErrorFilter } from "./helpers/filter-error.http";
 // import { TypeOrmFactory } from "@database/typeorm.factory";
-import { GlobalCatcher, HttpErrorFilter } from "@helpers/filter-error.http";
 // import { AuthModule } from "@modules/auth/auth.module";
 // import { UsersModule } from "@modules/users/users.module";
-
 import { ReportsController } from "./modules/reports/reports.controller";
 import { ReportsModule } from "./modules/reports/reports.module";
 import { NotionController } from "./services/notion/notion.controller";
